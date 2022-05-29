48929640d903669fe89c2930989cfdf3c8bcb580
Sathishka Priyad <spriyad.info@gmail.com>
2022-05-29 16:53:20 +0530

    Delete SathishkaSubasinghe/GuessingGame directory

--- SathishkaSubasinghe/GuessingGame/registration.php
+++
@@ -1,7 +0,0 @@
-<?php
-    \Magento\Framework\Component\ComponentRegistrar::register(
-        \Magento\Framework\Component\ComponentRegistrar::MODULE,
-        'SathishkaSubasinghe_GuessingGame',
-        __DIR__
-    );
-?>
